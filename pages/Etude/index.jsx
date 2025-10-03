6d025f6a01c3c600652aa2a20833b0ef7d62c43f
Gouba Desire <goubadesire386@gmail.com>
2025-10-03 01:08:21 +0000

    Correction import Layout et ajustement composant Etude

--- pages/Etude/index.jsx
+++ pages/Etude/index.jsx
@@ -1,4 +1,4 @@
-import Layout from "../../components/Layout";
+import Layout from "../../components/layout";
 import Head from "next/head";
 import Style from "./etude.module.css";
 import { useState, useEffect } from "react";
