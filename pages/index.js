8b2ea2001c64d50ceab78fa98aeeac4a79b995f2
Gouba Desire <goubadesire386@gmail.com>
2025-10-03 17:26:07 +0000

    Correction DATABASE_URL et login

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
+// pages/index.jsx
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
-
   useEffect(() => {
-    const token = localStorage.getItem("token"); // ou autre moyen d'auth
-    if (token) {
-      router.replace("/dashboard"); // connecté → dashboard
-    } else {
-      router.replace("/login"); // non connecté → login
-    }
+    router.replace("/dashboard"); // redirection immédiate (remplace l'historique)
   }, [router]);
 
-  return null;
+  return null; // ou un loader si tu veux
 }
