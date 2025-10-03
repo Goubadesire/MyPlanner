001b4a0efc336841c793a425bceddc5c66f0882b
Gouba Desire <goubadesire386@gmail.com>
2025-10-03 16:06:23 +0000

    Fix API login to use Supabase client

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
+// pages/index.jsx
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
-
   useEffect(() => {
-    router.replace("/login"); // redirection automatique vers la page login
+    router.replace("/dashboard"); // redirection immédiate (remplace l'historique)
   }, [router]);
 
-  return null;
+  return null; // ou un loader si tu veux
 }
